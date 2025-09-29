abe0afec08078875529bbf8f36603aea7658303e
Arkady Sokolenko <github@diskills-developer.org>
2025-09-29 20:41:30 +0300

    add simd

--- rust/polylines/src/lib.rs
+++ rust/polylines/src/lib.rs
@@ -42,31 +42,57 @@ fn decode(encoded: *const u8, len: usize, factor: i32) -> *mut f64  {
     path
 }
 
+#[inline(always)]
+unsafe fn simd_sub_64(arr: *mut v128, len: usize) {
+    let mut i = 0;
+    while i < len {
+        *arr.add(i) = i8x16_sub(*arr.add(i), i8x16_splat(64));
+        i += 1;
+    }
+}
+
+#[inline(always)]
+unsafe fn simd_get_offset(encoded: *const i8, index: &mut usize) -> i32 {
+    let mut result = 1;
+    let mut shift = 0;
+    let mut b: i32;
+    loop {
+        b = *encoded.add(*index) as i32;
+        *index += 1;
+        result += b << shift;
+        shift += 5;
+        if b < 0x1f {
+            break;
+        }
+    }
+    return if result & 1 != 0 { !(result >> 1) } else { result >> 1 };
+}
+
 #[no_mangle]
 #[target_feature(enable = "simd128")]
 pub unsafe extern "C"
-fn decode_simd(encoded: *const u8, len: usize, factor: f64) -> *mut f64  {
+fn decode_simd(encoded: *mut i8, len: usize, factor: f64) -> *mut f64 {
+    simd_sub_64(encoded as *mut v128, (len + 15) >> 4);
     let path: *mut v128 = encoded.add(((len + 7) & !7) + 8) as *mut v128;
 
     let mut index = 0;
     let mut point_index = 0;
-
     *path = f64x2(
-        decode_value(encoded, &mut index) as f64,
-        decode_value(encoded, &mut index) as f64,
+        simd_get_offset(encoded, &mut index) as f64,
+        simd_get_offset(encoded, &mut index) as f64,
     );
     while index < len {
         *path.add(point_index + 1) = f64x2_add(
             *path.add(point_index),
             f64x2(
-                decode_value(encoded, &mut index) as f64,
-                decode_value(encoded, &mut index) as f64,
+                simd_get_offset(encoded, &mut index) as f64,
+                simd_get_offset(encoded, &mut index) as f64,
             ),
         );
-        *path.add(point_index) = f64x2_div(*path.add(point_index), f64x2(factor, factor));
+        *path.add(point_index) = f64x2_div(*path.add(point_index), f64x2_splat(factor));
         point_index += 1;
     }
-    *path.add(point_index) = f64x2_div(*path.add(point_index), f64x2(factor, factor));
+    *path.add(point_index) = f64x2_div(*path.add(point_index), f64x2_splat(factor));
     point_index += 1;
 
     *((path as *mut f64).sub(1) as *mut usize) = point_index << 1;
