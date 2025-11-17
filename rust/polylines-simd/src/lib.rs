129894271b2dfcd512c34e4209c75f0901a0e808
Arkady Sokolenko <github@diskills-developer.org>
2025-11-17 18:05:02 +0300

    add simd

---
+++ rust/polylines-simd/src/lib.rs
@@ -0,0 +1,82 @@
+#![cfg(target_arch = "wasm32")]
+
+use std::arch::wasm32::*;
+
+#[inline]
+fn sub64(arr: *mut v128, len: usize) {
+    let blocks = len >> 4;
+
+    let mut i = 0;
+    while i < blocks {
+        unsafe {
+            *arr.add(i) = i8x16_sub(*arr.add(i), i8x16_splat(64));
+        }
+        i += 1;
+    }
+
+    i <<= 4;
+    let array = arr as *mut i8;
+    while i < len {
+        unsafe {
+            *array.add(i) -= 64;
+        }
+        i += 1;
+    }
+}
+
+#[inline]
+fn decode_value(encoded: *const i8, index: &mut usize) -> i32 {
+    let mut result = 1;
+    let mut shift = 0;
+    let mut b: i32;
+    loop {
+        unsafe {
+            b = *encoded.add(*index) as i32;
+        }
+        *index += 1;
+        result += b << shift;
+        shift += 5;
+        if b < 0x1f {
+            break;
+        }
+    }
+    return if result & 1 != 0 { !(result >> 1) } else { result >> 1 };
+}
+
+#[unsafe(no_mangle)]
+#[target_feature(enable = "simd128")]
+pub unsafe
+fn decode(encoded: *mut i8, len: usize, factor: f64) -> *mut f64 {
+    unsafe {
+        sub64(encoded as *mut v128, len);
+
+        let path: *mut v128 = encoded.add(((len + 7) & !7) + 8) as *mut v128;
+
+        let mut index = 0;
+        let mut point_index = 0;
+        *path = f64x2(
+            decode_value(encoded, &mut index) as f64,
+            decode_value(encoded, &mut index) as f64,
+        );
+        while index < len {
+            *path.add(point_index + 1) = f64x2_add(
+                *path.add(point_index),
+                f64x2(
+                    decode_value(encoded, &mut index) as f64,
+                    decode_value(encoded, &mut index) as f64,
+                ),
+            );
+            *path.add(point_index) = f64x2_div(
+                *path.add(point_index), f64x2_splat(factor),
+            );
+            point_index += 1;
+        }
+        *path.add(point_index) = f64x2_div(
+            *path.add(point_index), f64x2_splat(factor)
+        );
+        point_index += 1;
+
+        *((path as *mut f64).sub(1) as *mut usize) = point_index << 1;
+        path as *mut f64
+    }
+}
---
+++ rust/polylines-simd/wrapper.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const filename = `${__dirname}/target/wasm32-unknown-unknown/release/polylines_simd.wasm`;
+const bytes = fs.readFileSync(filename);
+
+let memory = null;
+let instance = null;
+
+async function init(mem = null) {
+    if (!mem) {
+        mem = new WebAssembly.Memory({ initial: 17 });
+    }
+    memory = mem;
+    obj = await WebAssembly.instantiate(
+        new Uint8Array(bytes), { env: { memory } },
+    );
+    instance = obj.instance;
+}
+
+function decode(str, factor = 1e5) {
+    const encodedStr = new TextEncoder().encode(str);
+    const encodedBuf = new Uint8Array(memory.buffer, 0, encodedStr.length);
+    encodedBuf.set(encodedStr);
+
+    const res = instance.exports.decode(0, encodedStr.length, factor);
+    const len = new Uint32Array(memory.buffer, res - 8, 1)[0];
+
+    const coordinates = new Float64Array(memory.buffer, res, len);
+    const path = new Array(len >> 1);
+    for (let i = 0, j = 0; i < len; j++, i += 2) {
+        path[j] = [coordinates[i], coordinates[i + 1]];
+    }
+    return path;
+}
+
+module.exports = { init, decode };
